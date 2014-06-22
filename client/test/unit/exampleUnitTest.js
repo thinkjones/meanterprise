cfb5e64ae19ccbaacf77f728e185f6336a0c201b
Gene ConroyJones <drgenejones@gmail.com>
2014-06-22 09:54:23 -0700

    Integrated Karma and Protractor into default Grunt task so that all unit tests and e2e tests can be run when a file changes.  Included two basic example tests which aren't related to testing the project but are purely for illustration purposes only.

---
+++ client/test/unit/exampleUnitTest.js
@@ -0,0 +1,7 @@
+describe('Example Unit Test', function() {
+    it('Should test for hello', function() {
+        var greeting = "Hello";
+
+        expect(greeting).toEqual('Hello');
+    });
+});
--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -3,57 +3,79 @@
 var request = require('request');
 
 module.exports = function (grunt) {
-  var reloadPort = 35729, files;
+    var reloadPort = 35729, files;
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    develop: {
-      server: {
-        file: 'server.js'
-      }
-    },
-    watch: {
-      options: {                              
-        nospawn: true,
-        livereload: reloadPort
-      },
-      js: {
-        files: [
-          'server.js',
-          '../client/src/*.js',
-          '../client/src/scripts/**/*.js',
-          '../client/src/styles/**/*.js',
-          'config/*.js'
-        ],
-        tasks: ['develop', 'delayed-livereload']
-      },
-      jade: {
-        files: ['views/**/*.jade'],
-        options: { livereload: reloadPort },
-      },
-    }
-  });
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        develop: {
+            server: {
+                file: 'server.js'
+            }
+        },
+        watch: {
+            options: {
+                nospawn: true,
+                livereload: reloadPort
+            },
+            js: {
+                files: [
+                    'server.js',
+                    '../client/src/*.js',
+                    '../client/src/scripts/**/*.js',
+                    '../client/src/styles/**/*.js',
+                    'config/*.js',
+                    '../client/test/**/*.js'
 
-  grunt.config.requires('watch.js.files');
-  files = grunt.config('watch.js.files');
-  files = grunt.file.expand(files);
+                ],
+                tasks: ['develop', 'delayed-livereload', 'test']
+            },
+            jade: {
+                files: ['views/**/*.jade'],
+                options: { livereload: reloadPort }
+            }
+        },
+        karma: {
+            unit: {
+                configFile: '../client/karma.conf.js'
+            }
+        },
 
-  grunt.registerTask('delayed-livereload', 'Live reload after the node server has restarted.', function () {
-    var done = this.async();
-    setTimeout(function () {
-      request.get('http://localhost:' + reloadPort + '/changed?files=' + files.join(','),  function(err, res) {
-          var reloaded = !err && res.statusCode === 200;
-          if (reloaded)
-            grunt.log.ok('Delayed live reload successful.');
-          else
-            grunt.log.error('Unable to make a delayed live reload.');
-          done(reloaded);
-        });
-    }, 500);
-  });
+        protractor: {
+            options: {
+                keepAlive: true,
+                configFile: "../client/protractor.conf.js"
+            },
+            run: {}
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-develop');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.config.requires('watch.js.files');
+    files = grunt.config('watch.js.files');
+    files = grunt.file.expand(files);
 
-  grunt.registerTask('default', ['develop', 'watch']);
+    grunt.registerTask('delayed-livereload', 'Live reload after the node server has restarted.', function () {
+        var done = this.async();
+        setTimeout(function () {
+            request.get('http://localhost:' + reloadPort + '/changed?files=' + files.join(','), function (err, res) {
+                var reloaded = !err && res.statusCode === 200;
+                if (reloaded)
+                    grunt.log.ok('Delayed live reload successful.');
+                else
+                    grunt.log.error('Unable to make a delayed live reload.');
+                done(reloaded);
+            });
+        }, 500);
+    });
+
+    grunt.loadNpmTasks('grunt-develop');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-protractor-runner');
+
+    grunt.registerTask('default', ['develop', 'watch']);
+
+    grunt.registerTask('test', [
+        'karma',
+        'protractor:run'
+    ]);
 };
